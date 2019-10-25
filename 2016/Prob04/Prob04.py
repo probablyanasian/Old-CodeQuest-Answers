392ab703d5c030608e2fe2e62f98f2bb32da32e4
ProbablyanAsian <43358667+probablyanasian@users.noreply.github.com>
2019-10-25 11:51:10 -0700

    Prob04, prep for rest

---
+++ 2016/Prob04/Prob04.py
@@ -0,0 +1,23 @@
+openFile = open("Prob04.in.txt", 'r')
+readFile = openFile.readlines()
+solutionFile = open("Prob04.solution.txt", "r+")
+solutionFile.seek(0)
+solutionFile.truncate()
+for line in range(int(readFile.pop(0).rstrip())):
+    inputLine = readFile.pop(0).rstrip()
+    inputWords = inputLine.split('|')
+    inputWords[0] = sorted(inputWords[0])
+    inputWords[1] = sorted(inputWords[1])
+
+    freqA = {}
+    for keys in inputWords[0]:
+        freqA[keys] = freqA.get(keys, 0) + 1
+
+    freqB = {}
+    for keys in inputWords[1]:
+        freqB[keys] = freqB.get(keys, 0) + 1
+    
+    if freqA == freqB:
+        solutionFile.write(inputLine + " = ANAGRAM\n")
+    else:
+        solutionFile.write(inputLine + " = NOT AN ANAGRAM\n")
